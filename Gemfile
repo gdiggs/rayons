55c8ff44ebba7c50c423f53fc887601c2d1a8db6
GordonDiggs <gordon@gordondiggs.com>
2013-02-23 16:51:58 -0500

    Initial rails commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+gem 'pg'
+gem 'thin'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'haml-rails'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem 'heroku'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rayons::Application.config.secret_token = 'fecc444d25154ac9f2fc00904d87968e2245829aa73b3e5c5e48cd934f86460132fe8e39e0cf107cf9cc722d6fc19b5ca8d26316c794e0a88a80318705241d6b'
