e3355f7044b2bd5fbc280b24b1b023a2582764b1
GordonDiggs <gordon@gordondiggs.com>
2013-09-08 13:37:10 -0400

    Need to load dependencies explicitly for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,5 @@ Rayons::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.dependency_loading = true if $rails_rake_task
 end
