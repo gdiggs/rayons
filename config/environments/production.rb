544ff197c569bae4ee4c7f2ad97adc6e67cf2add
GordonDiggs <gordon@gordondiggs.com>
2014-01-01 12:53:24 -0500

    Use mem cache store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,7 @@ Rayons::Application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.cache_store = :mem_cache_store
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
