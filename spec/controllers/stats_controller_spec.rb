e939234f424c6a78463b1618613873dad1ae89cb
GordonDiggs <gordon@gordondiggs.com>
2014-04-06 15:16:52 -0400

    Fix title test

--- spec/controllers/stats_controller_spec.rb
+++ spec/controllers/stats_controller_spec.rb
@@ -4,7 +4,7 @@ describe StatsController do
   describe '#index' do
     it 'should set title' do
       get :index
-      assert_select 'title', "Rayons\n| Stats"
+      assert_select 'title', "Rayons\n\u2669 Stats"
     end
   end
 
