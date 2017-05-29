c5be6c53abeb666d0628156a36834ea6b1b0e56d
GordonDiggs <gordon@gordondiggs.com>
2017-05-29 15:16:01 -0400

    Use rayons

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RayonsRewrite
+module Rayons
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
