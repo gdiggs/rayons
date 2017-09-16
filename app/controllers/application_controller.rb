7b3a80b59078cb4048ee85730e3202edaafedf02
GordonDiggs <gordon@gordondiggs.com>
2017-09-16 14:30:46 -0400

    Temporarily disable ssl force

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,6 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  force_ssl except: [:status], if: proc { Rails.env.production? }
-
   helper_method :default_error_response
 
   def blitz
