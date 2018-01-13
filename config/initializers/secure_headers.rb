8ff6981ac107cec4a9014a0f29da5f815e766d68
GordonDiggs <gordon@gordondiggs.com>
2018-01-13 15:29:11 -0500

    Close vulnerability on _blank links
    https://www.jitbit.com/alexblog/256-targetblank---the-most-underestimated-vulnerability-ever/

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,19 +1,17 @@
 SecureHeaders::Configuration.default do |config|
   config.csp.merge!(
-    {
-      script_src: %w[
-        'unsafe-eval'
-        'unsafe-inline'
-        'self'
-        d2wy8f7a9ursnm.cloudfront.net
-        www.gstatic.com
-      ],
-      style_src: %w[
-        'unsafe-inline'
-        'self'
-        fonts.googleapis.com
-        www.gstatic.com
-      ]
-    }
+    script_src: %w[
+      'unsafe-eval'
+      'unsafe-inline'
+      'self'
+      d2wy8f7a9ursnm.cloudfront.net
+      www.gstatic.com
+    ],
+    style_src: %w[
+      'unsafe-inline'
+      'self'
+      fonts.googleapis.com
+      www.gstatic.com
+    ],
   )
 end
