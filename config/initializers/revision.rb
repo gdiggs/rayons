d29639c3c9f63023a3e43fe4dbc69828e0944c94
Gordon Diggs <gordon@gordondiggs.com>
2018-11-21 16:59:55 -0500

    Check HEROKU_SLUG_COMMIT for revision
    Had to enable this to make it work: https://devcenter.heroku.com/articles/dyno-metadata

--- config/initializers/revision.rb
+++ config/initializers/revision.rb
@@ -1,5 +1,7 @@
 Rails.configuration.git_revision = if File.exist? Rails.root.join("REVISION")
                                      File.read(Rails.root.join("REVISION")).strip
+                                   elsif ENV["HEROKU_SLUG_COMMIT"]
+                                     ENV["HEROKU_SLUG_COMMIT"]
                                    else
                                      `git rev-parse HEAD`.strip
                                    end
