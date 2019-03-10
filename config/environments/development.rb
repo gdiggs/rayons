c561b6908ce97bcff78689844545080abd1b9dd7
Gordon Diggs <gordon@gordondiggs.com>
2019-03-10 14:01:50 -0400

    Log to STDOUT in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,6 +24,8 @@ Rayons::Application.configure do
 
   config.log_level = :debug
 
+  config.logger = ActiveSupport::TaggedLogging.new(ActiveSupport::Logger.new(STDOUT))
+
   config.action_mailer.preview_path = "#{Rails.root}/spec/mailers/previews"
 
   config.reload_classes_only_on_change = false
