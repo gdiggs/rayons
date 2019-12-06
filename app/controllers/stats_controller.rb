026dd84840eae6a7d335527aeea5f0ed284889ff
Gordon Diggs <gordon@gordondiggs.com>
2019-12-06 16:49:17 -0500

    Clear stats cache per deploy
    Fixes #297

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -11,7 +11,7 @@ class StatsController < ApplicationController
 
   # GET /stats
   def index
-    render html: (cache [:item_stats, Item.unscoped.maximum(:updated_at).to_i] do
+    render html: (cache [:item_stats, Item.unscoped.maximum(:updated_at).to_i, Rails.configuration.git_revision] do
       @prices = @item_stats.significant_prices
       render_to_string
     end)
