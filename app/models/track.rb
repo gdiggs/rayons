9b9b240447d9ed421b5bf96b8dacbd956b6ed02c
Gordon Diggs <gordon@gordondiggs.com>
2020-09-11 18:04:03 -0400

    Add possible covers and alternates

--- app/models/track.rb
+++ app/models/track.rb
@@ -5,6 +5,14 @@ class Track < ApplicationRecord
 
   default_scope { where(deleted: false) }
 
+  def possible_covers
+    self.class.where("name = ? AND artist != ?", name, artist)
+  end
+
+  def possible_alternates
+    self.class.where("name = ? AND artist = ? AND id != ?", name, artist, id)
+  end
+
   def destroy
     self.deleted = true
     save!
