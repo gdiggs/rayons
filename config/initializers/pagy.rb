d6817b59a8bb3d34172c515255ee20edc287b495
Gordon Diggs <gordon@gordondiggs.com>
2021-10-23 15:56:42 -0400

    Fix pagy breaking changes

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,5 +1,5 @@
 require "pagy/extras/bootstrap"
 
-Pagy::VARS[:items] = 100
-Pagy::VARS[:size] = [1, 20, 20, 1]
+Pagy::DEFAULT[:items] = 100
+Pagy::DEFAULT[:size] = [1, 20, 20, 1]
 Pagy::I18n.load(locale: "en", filepath: "config/locales/pagy-en.yml")
