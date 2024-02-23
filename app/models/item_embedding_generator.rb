84d0ec7745533c47bb89606bf2cf459d66d239a8
Gordon Diggs <gordon@gordondiggs.com>
2024-02-23 17:52:20 -0500

    Use same md5 source in both places

--- app/models/item_embedding_generator.rb
+++ app/models/item_embedding_generator.rb
@@ -7,12 +7,13 @@ class ItemEmbeddingGenerator
     openai = OpenaiWrapper.new
 
     Item.all.find_each do |item|
-      next if item.embedding_md5 == md5("#{item.as_embedding}/v#{EMBEDDING_VERSION}")
+      md5_str = "#{item.as_embedding}/v#{EMBEDDING_VERSION}"
+      next if item.embedding_md5 == md5(md5_str)
 
       Rails.logger.info("Embedding: #{item.artist} - #{item.title}")
       embedding = openai.create_embedding(item.as_embedding)
       item.embedding = embedding
-      item.embedding_md5 = md5(item.as_embedding)
+      item.embedding_md5 = md5(md5_str)
       item.save!
     end
   end
